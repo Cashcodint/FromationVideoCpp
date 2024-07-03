883321d15560f1342a4ce0acaf7776f2a69b026e
Cashcodint <poubell.439@gmail.com>
2024-07-03 11:38:13 +0200

    This is the first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+# include <fstream>
+# include <iostream>
+# include <string>
+# include "Utils/utils.hpp"
+
+struct Product
+{
+	std::string name;
+	float price;
+	bool available;
+};
+
+int main()
+{
+	
+
+	return 0;
+}
---
+++ utils.cpp
@@ -0,0 +1,8 @@
+// # include <cassert> : En dur, ne dépend pas de l'exécution du programme. Supprimer pour les release.
+# include <iostream>
+# include <string>
+
+void HelloWorld()
+{
+	std::cout << "Hello World" << std::endl;
+}
